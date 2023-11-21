056648c186e23fb9507ecf3eeaea342dd80d3ff4
oasis-uet <71420293+oasis-uet@users.noreply.github.com>
2023-11-21 13:41:17 +0700

    Update VirtualThreadExample.java

--- src/VirtualThreadExample.java
+++ src/VirtualThreadExample.java
@@ -4,12 +4,10 @@ import java.util.concurrent.Executors;
 
 public class VirtualThreadExample {
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newVirtualThreadExecutor();
-
+        ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
         executor.submit(() -> {
             System.out.println("Hello from a virtual thread!");
         });
-
         executor.shutdown();
     }
 }
